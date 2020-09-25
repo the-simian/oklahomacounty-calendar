c49b2c53c22d84534e1a02462951d6a5d7c0b954
the-simian <harlinjesse@gmail.com>
2020-09-25 01:26:07 -0500

    update file

--- helpers.js
+++ helpers.js
@@ -8,7 +8,40 @@ const titleCase = (str) => {
   return str.join(" ");
 };
 
+const URL = `https://oklahomacounty.legistar.com/Calendar.aspx`;
+
+const NAME = "Name";
+const DATE = "Meeting Date";
+const TIME = "Meeting Time";
+const LOCATION = "Meeting Location";
+const DETAILS = "Meeting Details";
+const AGENDA = "Agenda";
+const MINUTES = "Minutes";
+const VIDEO = "Video";
+
+const colDefinition = [
+  { name: NAME, type: "text" },
+  { name: DATE, type: "text" },
+  { name: "", type: "none" },
+  { name: TIME, type: "text" },
+  { name: LOCATION, type: "text" },
+  { name: DETAILS, type: "link" },
+  { name: AGENDA, type: "link" },
+  { name: MINUTES, type: "link" },
+  { name: VIDEO, type: "link" },
+];
+
 module.exports = {
+  URL,
+  NAME,
+  DATE,
+  TIME,
+  LOCATION,
+  DETAILS,
+  AGENDA,
+  MINUTES,
+  VIDEO,
   hours,
   titleCase,
+  colDefinition,
 };
--- scrape.js
+++ scrape.js
@@ -1,27 +1,17 @@
-const { hours, titleCase } = require('./helpers')
+const {   URL,  
+    NAME,
+    DATE,
+    TIME,
+    LOCATION,
+    DETAILS,
+    AGENDA,
+    MINUTES,
+    VIDEO,
+    hours,
+    titleCase,
+    colDefinition } = require('./helpers')
 
-const URL = `https://oklahomacounty.legistar.com/Calendar.aspx`;
 
-const NAME = "Name";
-const DATE = "Meeting Date";
-const TIME = "Meeting Time";
-const LOCATION = "Meeting Location";
-const DETAILS = "Meeting Details";
-const AGENDA = "Agenda";
-const MINUTES = "Minutes";
-const VIDEO = "Video";
-
-const colDefinition = [
-  { name: NAME, type: "text" },
-  { name: DATE, type: "text" },
-  { name: "", type: "none" },
-  { name: TIME, type: "text" },
-  { name: LOCATION, type: "text" },
-  { name: DETAILS, type: "link" },
-  { name: AGENDA, type: "link" },
-  { name: MINUTES, type: "link" },
-  { name: VIDEO, type: "link" },
-];
 
 const li = (item, prop) => {
     let _li = "";
